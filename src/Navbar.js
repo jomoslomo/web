6b775a72b8a589409835844990f6ea838d8cfc5e
JM <jomo@3c-06-30-2d-1a-5d.wlan.lsu.edu>
2023-05-04 13:13:35 -0500

    updated site

---
+++ src/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Navbar.css';
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <NavLink exact to="/" className="nav-link">
+            Hi, I'm Jony.
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink exact to="./Projects" className="nav-link">
+            Projects
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <a href="/jomoresume.pdf" download="JonathanMorseCV.pdf" className="nav-link">
+            Download CV
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Projects.css';
+
+function Projects() {
+  return (
+    <div>
+      <h1>Here's some cool stuff I've made...</h1>
+      <ul>
+        <li><a href="https://jomoslomo.github.io/dmae/public/index.html">Chromatik</a></li>
+        <li><a href="https://stocksim.duckdns.org">Stock Sim</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
