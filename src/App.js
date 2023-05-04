cd9d0c209a952c1088323ad73cdfc8e28a42402a
JM <jomo@3c-06-30-2d-1a-5d.wlan.lsu.edu>
2023-05-04 13:54:30 -0500

    fixed landing page issue

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/web" element={<Landing />} />
         <Route path="/projects" element={<Projects />} />
 
       </Routes>
