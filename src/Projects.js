3c3d429870144b0859d51ad2b7ca4fc541866fd0
JM <jomo@3c-06-30-2d-1a-5d.wlan.lsu.edu>
2024-02-01 12:25:16 -0600

    updated links

--- src/Projects.js
+++ src/Projects.js
@@ -2,34 +2,49 @@ import React from 'react';
 import './Projects.css';
 
 function Projects() {
+  const projectsData = [
+    {
+      name: 'Chromatik',
+      url: 'https://jomoslomo.github.io/dmae/public/index.html',
+    },
+    {
+      name: 'Stock Sim',
+      url: 'https://jomoslomo.github.io/stocksim/index.html',
+    },
+    {
+      name: 'Sound 2 Plants',
+      url: 'https://jomoslomo.github.io/s2pv1/s2p/p5rg/index.html',
+    },
+    {
+      name: 'Jake The Dog',
+      url: 'https://jomoslomo.github.io/jakethedog/index.html',
+    },
+    {
+      name: 'Ascii Portrait',
+      url: 'https://jomoslomo.github.io/asciiportrait/index.html',
+    },
+    {
+      name: 'Self Portrait',
+      url: 'https://jomoslomo.github.io/self-portrait/index.html',
+    },
+  ];
+
   return (
     <div>
       <h1>Here's some cool stuff I've made...</h1>
-      <ul>
-        <li>
-          <div className="project-link">
-            <a href="https://jomoslomo.github.io/dmae/public/index.html">Chromatik</a>
-          </div>
-          <div className="project-iframe">
-            <iframe src="https://jomoslomo.github.io/dmae/public/" title="Chromatik"></iframe>
-          </div>
-        </li>
-        <li>
-          <div className="project-link">
-            <a href="https://stocksim.duckdns.org">Stock Sim</a>
-          </div>
-          <div className="project-iframe">
-            <iframe src="https://stocksim.duckdns.org" title="Stock Sim"></iframe>
-          </div>
-        </li>
-        <li>
-          <div className="project-link">
-            <a href="https://jomoslomo.github.io/s2pv1/s2p/p5rg/index.html">Sound 2 Plants </a>
-          </div>
-          <div className="project-iframe">
-            <iframe src="https://jomoslomo.github.io/s2pv1/s2p/p5rg/" title="Sound 2 Plants"></iframe>
-          </div>
-        </li>
+      <ul className="projects-list">
+        {projectsData.map((project, index) => (
+          <li className="project" key={index}>
+            <div className="project-link">
+              {/* Update the <a> tag here */}
+              <a href={project.url} target="_blank" rel="noopener noreferrer">{project.name}</a>
+            </div>
+            {/* Consider if you also want the iframe to load a site in a new tab, which is not standard behavior */}
+            <div className="project-iframe">
+              <iframe src={project.url} title={project.name}></iframe>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
